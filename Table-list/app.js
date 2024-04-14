810b3daece8c5d9ca1a240503163f5c0cc75de6a
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-14 15:44:06 +0200

    table filter

--- Table-list/app.js
+++ Table-list/app.js
@@ -4,34 +4,43 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      gender: "male",
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      gender: "female",
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      gender: "female",
     },
     {
       id: 4,
       age: 24,
       name: "Karol",
+      gender: "male",
     },
   ],
 };
 
 const Item = ({ user }) => (
-  <div>
-    <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age}</h2>
+  <div className="userInfo">
+    <h1>{user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <p>
+      Wiek użytkownika: <strong>{user.age}</strong>
+    </p>
+    <p>Płeć użytkownika: {user.gender}</p>
   </div>
 );
 class ListItems extends React.Component {
   render() {
-    const users = this.props.data.users;
+    let users = this.props.data.users;
+    users = users.filter((user) => user.gender === "male");
     const Items = users.map((user) => <Item key={user.id} user={user} />);
 
     return <ul>{Items}</ul>;
