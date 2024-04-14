4fbf20e29e2d603f2494ae38c794cbaa5e7b6e90
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-14 18:26:49 +0200

    list filter

--- Table-list/app.js
+++ Table-list/app.js
@@ -38,12 +38,47 @@ const Item = ({ user }) => (
   </div>
 );
 class ListItems extends React.Component {
-  render() {
-    let users = this.props.data.users;
-    users = users.filter((user) => user.gender === "male");
-    const Items = users.map((user) => <Item key={user.id} user={user} />);
+  state = {
+    select: "all",
+  };
 
-    return <ul>{Items}</ul>;
+  handleUsersFilter(option) {
+    this.setState({
+      select: option,
+    });
+  }
+
+  usersList = () => {
+    let users = this.props.data.users;
+    switch (this.state.select) {
+      case "all":
+        return users.map((user) => <Item key={user.id} user={user} />);
+      case "female":
+        users = users.filter((user) => user.gender === "female");
+        return users.map((user) => <Item key={user.id} user={user} />);
+      case "female":
+        users = users.filter((user) => user.gender === "male");
+        return users.map((user) => <Item key={user.id} user={user} />);
+      default:
+        return "Coś nie działa";
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleUsersFilter.bind(this, "all")}>
+          Wszyscy
+        </button>
+        <button onClick={this.handleUsersFilter.bind(this, "female")}>
+          Kobiety
+        </button>
+        <button onClick={this.handleUsersFilter.bind(this, "male")}>
+          Mężczyźni
+        </button>
+        {this.usersList()}
+      </div>
+    );
   }
 }
 
