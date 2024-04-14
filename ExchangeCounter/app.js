36db51062290f4d81f95bb937b35b3c450080202
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-14 22:30:54 +0200

    optymalization

--- ExchangeCounter/app.js
+++ ExchangeCounter/app.js
@@ -1,14 +1,3 @@
-// const Dollars = (props) => (
-//   <div>Wartość w dolarach: {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
-// )
-
-// const Euros = (props) => {
-//   const value = (props.cash / props.ratio).toFixed(2)
-//   return (
-//     <div>Wartość w euro: {props.cash <= 0 ? "" : value}</div>
-//   )
-// }
-
 const Cash = (props) => {
   const value = (props.cash / props.ratio).toFixed(2);
   return (
@@ -21,9 +10,36 @@ const Cash = (props) => {
 class ExchangeCounter extends React.Component {
   state = {
     amount: "",
-    ratioDollar: 3.6,
-    ratioEuro: 4.2,
+    // ratioDollar: 3.6,
+    // ratioEuro: 4.2,
   };
+
+  currencies = [
+    {
+      id: 1,
+      name: "dolar",
+      ratio: 3.6,
+      title: "Wartość w dolarach: ",
+    },
+    {
+      id: 2,
+      name: "euro",
+      ratio: 4.2,
+      title: "Wartość w euro: ",
+    },
+    {
+      id: 3,
+      name: "frank",
+      ratio: 4.41,
+      title: "Wartość w frankach szwajcarskich: ",
+    },
+    {
+      id: 4,
+      name: "funt",
+      ratio: 5.0,
+      title: "Wartość w funtach brytyjskich: ",
+    },
+  ];
 
   handleChange = (e) => {
     this.setState({
@@ -32,10 +48,20 @@ class ExchangeCounter extends React.Component {
   };
 
   render() {
-    const { amount, ratioDollar, ratioEuro } = this.state;
+    const { amount } = this.state;
+
+    const calculators = this.currencies.map((currency) => (
+      <Cash
+        key={currency.id}
+        cash={amount}
+        ratio={currency.ratio}
+        title={currency.title}
+      />
+    ));
 
     return (
       <div className="app">
+        <h1>Exchange Counter</h1>
         <label>
           <input
             type="number"
@@ -43,10 +69,11 @@ class ExchangeCounter extends React.Component {
             onChange={this.handleChange}
           />
         </label>
-        {/* <Dollars cash={amount} ratio={ratioDollar} /> */}
-        {/* <Euros cash={amount} ratio={ratioEuro} /> */}
-        <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
-        <Cash cash={amount} ratio={ratioEuro} title="Wartość w euro: " />
+
+        {calculators}
+
+        {/* <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
+        <Cash cash={amount} ratio={ratioEuro} title="Wartość w euro: " /> */}
       </div>
     );
   }
